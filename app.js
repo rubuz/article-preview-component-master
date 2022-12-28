d780280c8b36b5693f9526db06d09414b1c7bcf9
Matjaz Lovenjak <rubuz23@gmail.com>
2022-12-28 12:31:17 +0100

    desktop popup added, need to fix overflow

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const show = document.querySelector(".show__popup");
 const hide = document.querySelector(".hide__popup");
 
 show.addEventListener("click", () => {
-  popup.classList.remove("hidden");
+  popup.classList.toggle("hidden");
 });
 
 hide.addEventListener("click", () => {
